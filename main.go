85f6fc0b108afa8625443ef2a7264a6e02037409
bodaay <khalefa@al-ahmad.com>
2020-05-23 00:21:27 +0300

    nice

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
 
 func main() {
+	convertToOpus("test/audio/16k-54s.wav")
 }
---
+++ opus/LinuxBuildAll.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+sudo apt-get install git autoconf automake libtool libssl-dev gcc make
+
+current=$PWD
+outputFolder="$current/lib/linux/x64"
+mkdir -p $outputFolder
+
+echo "Building libogg library"
+cd $current
+cd src/libogg
+./autogen.sh
+./configure --disable-doc --disable-examples
+make clean
+make
+cp ./src/.libs/*.so ./src/.libs/*.a $outputFolder/
+# make clean
+echo "Finished Building libogg library"
+
+
+echo "Building Opus Core library"
+cd $current
+cd src/opus
+./autogen.sh
+./configure --disable-doc --disable-examples
+make clean
+make
+cp ./.libs/*.so ./.libs/*.a $outputFolder/
+# make clean
+echo "Finished Building Opus Core library"
+
+echo "Building Opus File library"
+cd $current
+cd src/opus-file
+./autogen.sh
+DEPS_LIBS="-L$outputFolder" DEPS_CFLAGS="-I$current/src/opus/include -I$current/src/libogg/include" ./configure --disable-http  --disable-doc --disable-examples --disable-http
+make clean
+make
+cp ./.libs/*.so ./.libs/*.a $outputFolder/
+# make clean
+echo "Finished Building Opus File library"
+
+
+echo "Building Opus libopusenc library"
+cd $current
+cd src/opus-libopusenc
+./autogen.sh
+DEPS_LIBS="-L$outputFolder" DEPS_CFLAGS="-I$current/src/opus/include  -I$current/src/libogg/include" ./configure  --disable-doc --disable-examples
+make clean
+make
+cp ./.libs/*.so ./.libs/*.a $outputFolder/
+# make clean
+echo "Finished Building Opus libopusenc library"
+
+
+
